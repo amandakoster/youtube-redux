9e4c99ee791094c8b39abdcacef59c6aad3d7408
Amanda Koster <amandakoster@gmail.com>
2017-12-30 13:35:00 -0800

    scaf

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+const API_KEY= "AIzaSyDrMKV08QY0agCAddJvXYkrraaFANuRqDg"
 
-export default class App extends Component {
+
+class App extends Component {
   render() {
     return (
       <div>React simple starter</div>
     );
   }
 }
+
+export default App;
