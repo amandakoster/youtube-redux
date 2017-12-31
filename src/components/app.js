1de720ce5730c5f1d71f4a3abaa809c715a21745
Amanda Koster <amandakoster@gmail.com>
2017-12-30 17:17:11 -0800

    video player works for all videos

--- src/components/app.js
+++ src/components/app.js
@@ -5,26 +5,34 @@ import VideoDetail from './video-detail';
 import SearchBar from './searchbar';
 import VideoList from './video-list';
 
-
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {videos: []}
 
-    YTSearch({key:API_KEY, term: 'cats'}, (videos) => {
-      this.setState({ videos });
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key:API_KEY, term: 'black holes'}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
-
 
   render() {
     return (
       <div>
 
-      <div>YoutTube app with React and Redux</div>
-      <SearchBar />
-      <VideoDetail video={this.state.videos[0]} />
-      <VideoList videos={this.state.videos} />
+        <div>YouTube app with React and Redux</div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+        onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        videos={this.state.videos} />
+
       </div>
     );
   }
--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const VideoListItem = (props) => {
     const video = props.video
+    const onVideoSelect = props.onVideoSelect;
     const imageURL = video.snippet.thumbnails.default.url
     
     return(
         
-        <li className="list-group-item">
+        <li onClick={() => onVideoSelect(video)}
+            className="list-group-item">
+
             <div className="video-list media">
                 <div className="media-left">
                     <img className="media-object" src={imageURL}/>
@@ -15,6 +18,7 @@ const VideoListItem = (props) => {
                         <div className="media-heading">{video.snippet.title}</div>
                     </div>
             </div>
+
         </li>
 
     )
